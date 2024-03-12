6480ab42da7c344257db080b5354d8659beea32d
kforsl <forslund95@live.com>
2024-03-12 20:00:08 +0100

    end of day

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-document.querySelector(`.btn`).addEventListener(`click`, openShoppingCart)
+// document.querySelector(`.NAV_SHOPPING_CART`).addEventListener(`click`, openShoppingCart)
+// document.querySelectorAll(`.ADD-BUTTON_DRINK_MENU`).forEach(item => item.addEventListener(`click`, changeCartValue))
 
 function openShoppingCart() {
     const modalRef = document.querySelector(`.shopping`)
