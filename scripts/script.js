a9e6071c9b60667662ec2c146481b28d1425801f
Lennstorm <4824136@gmail.com>
2024-03-18 23:06:58 +0100

    lagt tillbaka kod som försvann vid senaste merge

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 import {
     addLocalStorage,
     getLocalStorage,
+    addUsersLocalStorage,
 } from "./localStorage.js";
 
 import {
@@ -9,53 +10,17 @@ import {
     renderShoppingCart,
 } from "./render.js";
 
-const cart = [
-    {
-        "id": 1,
-        "title": "Bryggkaffe",
-        "price": 29,
-        "inCart": 2
-    },
-    {
-        "id": 2,
-        "title": "Caffè Doppio",
-        "price": 59,
-        "inCart": 1
-    },
-    {
-        "id": 3,
-        "title": "Cappuccino",
-        "price": 49,
-        "inCart": 0
-    },
-    {
-        "id": 4,
-        "title": "Latte Macchiato",
-        "price": 49,
-        "inCart": 0
-    },
-    {
-        "id": 5,
-        "title": "Kaffe Latte",
-        "price": 59,
-        "inCart": 3
-    },
-    {
-        "id": 6,
-        "title": "Cortado",
-        "price": 55,
-        "inCart": 0
-    },
-    {
-        "id": 7,
-        "title": "Santos Special",
-        "price": 69,
-        "inCart": 0
-    }
-]
+import {
+    fetchProducts,
+} from "./fetch.js";
 
-addLocalStorage(`cart`, cart);
-renderShoppingModal();
+addUsersLocalStorage()
+renderShoppingModal()
+
+if (window.location.pathname === "/product-page.html") {
+    document.querySelector(`.img-header-bag-icon`).addEventListener(`click`, openShoppingCart)
+    document.querySelectorAll(`.img-add-icon`).forEach(item => item.addEventListener(`click`, changeCartValue))
+}
 
 document.querySelector(`.main__nav-icon`).addEventListener(`click`, () => {
     const iconRef = document.querySelector(`.main__nav-icon`)
@@ -74,6 +39,15 @@ document.querySelector(`.main__nav-icon`).addEventListener(`click`, () => {
     renderNavLinks();
 })
 
+function openShoppingCart() {
+    const modalRef = document.querySelector(`.shopping`)
+    if (modalRef.open) {
+        modalRef.open = false;
+    } else {
+        modalRef.open = true;
+    }
+}
+
 function countItemPrice(price, quantity) {
     return price * quantity;
 }
@@ -88,27 +62,54 @@ function countTotalPrice(cart) {
     return totalPrice;
 }
 
-function changeCartValue() {
-    const cart = getLocalStorage(`cart`);
-    if (this.alt.includes(`Add`)) {
-        cart.forEach(item => {
+async function changeCartValue() {
+    if (!getLocalStorage(`cart`)) {
+        const products = await fetchProducts()
+        const cart = []
+        products.menu.forEach(item => {
+
             if (item.id === Number(this.dataset.id)) {
-                if (item.inCart > 0) {
-                    item.inCart++;
+                const cartItem = {
+                    "id": item.id,
+                    "title": item.title,
+                    "price": item.price,
+                    "inCart": 1,
                 }
+                cart.push(cartItem)
+            } else {
+                const cartItem = {
+                    "id": item.id,
+                    "title": item.title,
+                    "price": item.price,
+                    "inCart": 0,
+                }
+                cart.push(cartItem)
             }
         });
-    } else if (this.alt.includes(`Remove`)) {
-        cart.forEach(item => {
-            if (item.id === Number(this.dataset.id)) {
-                if (item.inCart > 0) {
-                    item.inCart--;
+        addLocalStorage(`cart`, cart);
+        renderShoppingCart()
+    } else {
+        const cart = getLocalStorage(`cart`);
+        if (this.alt.includes(`Add`)) {
+            cart.forEach(item => {
+                if (item.id === Number(this.dataset.id)) {
+                    if (item.inCart >= 0) {
+                        item.inCart++;
+                    }
                 }
-            }
-        });
+            });
+        } else if (this.alt.includes(`Remove`)) {
+            cart.forEach(item => {
+                if (item.id === Number(this.dataset.id)) {
+                    if (item.inCart >= 0) {
+                        item.inCart--;
+                    }
+                }
+            });
+        }
+        addLocalStorage(`cart`, cart);
+        renderShoppingCart();
     }
-    addLocalStorage(`cart`, cart);
-    renderShoppingCart();
 }
 
 function checkUserRole() {
@@ -118,7 +119,6 @@ function checkUserRole() {
     } else {
         return currentUser.role
     }
-
 }
 
 export {
@@ -128,35 +128,117 @@ export {
     checkUserRole,
     statusPageUpdate,
 };
-function statusPageUpdate () {
-    const uniqueOrderNr = '#12345'; 
+
 // Här ska det implementeras värde från funktion som skrivs senare.
+function statusPageUpdate() {
+    console.log(`test`);
+    const uniqueOrderNr = '#12345';
+    // Här ska det implementeras värde från funktion som skrivs senare.
     const orderNrRef = document.querySelector(".status__orderNr")
     let orderNr = uniqueOrderNr;
     let orderNrText = orderNrRef.textContent;
     orderNrText = orderNrText.replace("[ordernr]", orderNr);
     orderNrRef.textContent = orderNrText;
-
     const delivCountRef = document.querySelector(".status__delivCounter");
     let deliveryTime = 30;
-// ^ Här ska det istället för en siffra implementeras värde från funktion som skrivs senare.
+    // ^ Här ska det istället för en siffra implementeras värde från funktion som skrivs senare.
     let counterText = delivCountRef.textContent;
     counterText = counterText.replace("[nr]", deliveryTime);
     delivCountRef.textContent = counterText;
 }
+
 // Simpel funktion för att slumpa tiden för leveransen. Mellan 13 och 20 minuter.
 // Körs varje gång sidan laddas om.
 
 function renderDeliveryTime() {
-    var minuter = Math.floor(Math.random() * (20 - 13 + 1)) + 13; 
-    document.getElementById("deliveryCounter").innerHTML = "<strong>" + minuter + "</strong> minuter"; 
-  }
-  
+    var minuter = Math.floor(Math.random() * (20 - 13 + 1)) + 13;
+    document.getElementById("deliveryCounter").innerHTML = "<strong>" + minuter + "</strong> minuter";
+}
 
-  renderDeliveryTime();
+// Här börjar funktionen för att generera unikt ordernummer.
+function generateUniqueOrderNumber() {
+    const orderArray = []
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    let orderNumber = '#';
 
- 
+    for (let i = 0; i < 2; i++) {
+        orderNumber += letters.charAt(Math.floor(Math.random() * letters.length));
+    }
 
-    
+    for (let i = 0; i < 10; i++) {
+        orderNumber += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    }
+    if (getLocalStorage('orderNumbers')) {
+        const uniqueOrders = getLocalStorage('orderNumbers')
+        uniqueOrders.forEach(item => {
+            orderArray.push(item)
+        })
+    }
+    if (orderArray.includes(orderNumber)) {
+        generateUniqueOrderNumber()
+
+    } else {
+        orderArray.push(orderNumber)
+        addLocalStorage('orderNumbers', orderArray)
+    }
+
+}
+
+renderDeliveryTime();
+
+//ˇˇˇˇˇˇ Den här funktionen får gärna flyttas upp senare när vi går igenom och snyggar till!!!
+window.onload = function() {    
+    if (window.location.pathname.endsWith("login.html")) {
+        checkLoginDetails();
+    }
 };
+//^^^^^^^ Den här funktionen får gärna flyttas upp senare när vi går igenom och snyggar till!!!
 
+function checkLoginDetails() {
+    let logInBtnRef = document.querySelector('.login-page__submit-btn');
+    logInBtnRef.addEventListener('click', (event) => {
+        event.preventDefault();
+        validateLogin();
+    })
+}
+
+function validateLogin() {
+    try {
+        const userNameRef = document.querySelector('#loginEmail');
+        const passwordRef = document.querySelector('#loginPassword');
+        let descriptionRef = document.querySelector('.login-page__form-description');
+        const checkboxRef = document.querySelector('.login-page__check-container input[type="checkbox"]');
+
+        const users = getLocalStorage("users");
+        if (!users) {
+            throw "No users found in localStorage!"
+        }
+
+        const user = users.find(user => user.email === userNameRef.value);
+        console.log(user);
+
+        // if(!users.some(user => user.username === userNameRef.value)){
+        if(!user) {            
+            descriptionRef.innerText = 'Kontrollera användarnamn!';
+            userNameRef.focus();        
+        } else {
+            // const user = users.find(user => user.username === userNameRef.value);
+            if(user.password !== passwordRef.value) {
+                descriptionRef.innerText = 'Kontrollera lösenord!'
+                passwordRef.focus();
+            } else {
+                if (!checkboxRef.checked) {
+                    descriptionRef.innerText = 'Godkänn GDPR!';
+                    checkboxRef.focus()
+                } else {
+                    descriptionRef.innerText ='Logga in på ditt konto nedan för att se din orderhistorik.';
+                    addLocalStorage("currentUser", user)                    
+                    window.location.href = 'product-page.html';
+                }
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
